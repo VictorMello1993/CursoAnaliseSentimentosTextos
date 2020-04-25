79164676041959a6224c219bc25626e2df84beb6
Victor Mello <victorsmello93@gmail.com>
2020-04-25 14:53:27 -0300

    Implementado o treinamento do algoritmo utilizando Naive Bayes

--- mineracao.py
+++ mineracao.py
@@ -1,4 +1,6 @@
 import nltk
+import numpy as np
+import pandas as pd
 
 base = [('eu sou admirada por muitos','alegria'),
         ('me sinto completamente amado','alegria'),
@@ -21,7 +23,7 @@ base = [('eu sou admirada por muitos','alegria'),
         ('eu tenho muito medo dele', 'medo'),
         ('estou com medo do resultado dos meus testes', 'medo')]
 
-print(base[0]) #Primeira frase
+# print(base[0]) #Primeira frase
 
 stopwords = ['a', 'agora', 'algum', 'alguma', 'aquele', 'aqueles', 'de', 'deu', 'do', 'e', 'estou', 'esta', 'esta',
              'ir', 'meu', 'muito', 'mesmo', 'no', 'nossa', 'o', 'outro', 'para', 'que', 'sem', 'talvez', 'tem', 'tendo',
@@ -39,10 +41,10 @@ def removeStopWords(texto):
         frases.append((semstop, emocao))
     return frases
 
-print(removeStopWords(base))
+# print(removeStopWords(base))
 
 #Aplica stemming de palavras presentes numa base de dados com stopwords removidos
-def stemming(texto):
+def stemming1(texto):
     stemmer = nltk.stem.RSLPStemmer() #Stemmizador utilizado na língua portuguesa
     frasesstemming = []
     for (palavras, emocao) in texto:
@@ -50,8 +52,17 @@ def stemming(texto):
         frasesstemming.append((comstemming, emocao))
     return frasesstemming
 
-frasesstemming = stemming(base)
-print(frasesstemming)
+#Aplica stemming de palavras presentes numa base de dados sem remover stopwords, para evitar que o algoritmo de Naive Bayes não encontre correspondência na tabela de probabilidades
+def stemming2(texto):
+    frasesstemming = []
+    stemmer = nltk.stem.RSLPStemmer() #Stemmizador utilizado na língua portuguesa
+    for palavras in texto.split():        
+        comstemming = [str(stemmer.stem(p)) for p in palavras.split()]        
+        frasesstemming.extend(comstemming)
+    return frasesstemming
+
+frasesstemming = stemming1(base)
+# print(frasesstemming)
 
 #Obtendo todas as palavras de uma lista de stemming
 def buscaPalavras(frases):
@@ -61,36 +72,70 @@ def buscaPalavras(frases):
     return todasPalavras
 
 palavras = buscaPalavras(frasesstemming)
-print(palavras)
+# print(palavras)
 
 def buscaFrequenciaPalavras(palavras):
     palavras = nltk.FreqDist(palavras)
     return palavras
 
 frequencia = buscaFrequenciaPalavras(palavras)
-print(frequencia.most_common(50)) #Obtendo a frequência das primeiras 50 palavras
+# print(frequencia.most_common(50)) #Obtendo a frequência das primeiras 50 palavras
 
 def buscaPalavrasUnicas(frequencia):
     freq = frequencia.keys() #Buscando pela chave da tupla obtida na função BuscaFrequencia que é única para cada registro
     return freq
 
 palavrasUnicas = buscaPalavrasUnicas(frequencia)
-print(palavrasUnicas)
 
 #Extraindo as palavras presentes em uma determinada frase - retorna um dicionário de características onde a chave corresponde uma palavra, e o valor representa um booleano onde será setado como True se a palavra está presente numa frase ou False se não está presente.
 def extratorPalavras(documento):
     doc = set(documento)
     dict_caracteristicas = {}
     for palavras in palavrasUnicas:
-        dict_caracteristicas['%s' % palavras] = {palavras in doc}
+        dict_caracteristicas['%s' % palavras] = (palavras in doc)
     return dict_caracteristicas
 
 caracteristicasFrases  = extratorPalavras(['am', 'nov', 'dia'])
-print(caracteristicasFrases)
+# print(caracteristicasFrases)
 
 #Obtendo uma base de dados completa
 baseCompleta = nltk.classify.apply_features(extratorPalavras, frasesstemming)
-print(baseCompleta)
+# print(baseCompleta)
+
+# print(np.asarray(baseCompleta).shape) #Verificando o tamanho da base (neste caso, a base possui 20 linhas com 2 colunas, totalizando 40 elementos)
+tamanho_base = sum(len(i) for i in baseCompleta)
+
+baseReshape = np.reshape(baseCompleta, tamanho_base) #Base de dados redimensionada para melhor visualização
+# print(baseReshape)
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------Treinamento do modelo utilizando Naive Bayes-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+classificador = nltk.NaiveBayesClassifier.train(baseCompleta) #Construindo a tabela de probabilidades
+# print(classificador.labels()) #Obtendo as classes 
+
+# print(classificador.show_most_informative_features(5)) 
+'''Obtendo 5 características nos quais os atributos (palavras) são os mais representativos. Por exemplo, se o dia estiver presente em uma frase, significa que a probabilidade de uma
+frase ser de alegria é 2.3 vezes maior do que de medo. Porém, esses valores são menores porque a base de dados é menor, tem poucos registros.'''
+
+# teste = 'estou com medo'
+teste = 'amor medo dia apavorado'
+testestemming = stemming2(teste)
+print(testestemming)
+
+novo = extratorPalavras(testestemming) #Obtendo um registro de características da frase stemmizada 'est com med'
+# print(novo)
+
+#Efetivamente está realizando a classificação da frase em questão (label). O método classify() é responsável pelos cálculos de estimativas de probabilidades para cada classe e verificar a maior delas
+print(classificador.classify(novo))
+
+#Verificando as probabilidades de cada classe - Retorna label junto com as probabilidades
+distribuicao = classificador.prob_classify(novo)
+for classe in distribuicao.samples():
+    print(f'{classe}: {distribuicao.prob(classe)} %')
+
+
+'''OBS: O Stemming da NLTK não considera o contexto da frase, e sim palavra por palavra. Por esse motivo, existem frases, como 'eu te amo', em que 
+a palavra 'amo' não foi extraída para obter o seu radical, o que acabou influenciando no resultado final dos cálculos de probabilidades.
+Em vez de interpretar a frase como alegria, acabou interpretando como medo. A alternativa para melhorar os resultados é buscar um 
+outro stemmizador que trabalhe com mais precisão com essas palavras.'''
